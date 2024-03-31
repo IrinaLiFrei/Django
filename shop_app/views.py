4bbff3bec5518f8b24e069c7739aeef39a536951
Irina <airin-lia@mail.ru>
2024-03-31 16:15:18 +0300

    Initial commit

---
+++ shop_app/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+def index(request):
+    logger.info('Index page accessed')
+    return HttpResponse('Welcome to my SHOP!')
+
+
+def about(request):
+    logger.debug('About page accessed')
+    return HttpResponse('This is the ABOUT page.')
+
+
+def contact(request):
+    logger.debug('Contact page accessed')
+    return HttpResponse('This is the CONTACT page.')
+
+
+
